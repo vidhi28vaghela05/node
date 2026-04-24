51b8079daaa5a5778187fe557b92f487fe5b34df
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-04-24 11:26:34 +0530

    msg

---
+++ login_systems/BACKEND/models/product.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+let ProductSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        minLength: 10
+    },
+    description: {
+  type: String,
+        required: true,
+        minLength: 10
+    },
+    stock:{
+          type: Number,
+        required: true,
+        minLength: 0
+    },
+    price:{
+          type: Number,
+        required: true,
+        minLength: null
+    },
+    discount:{
+          type: Number,
+        required: true,
+        minLength: 0
+    },
+    isNewProduct:{
+          type: Boolean,
+        default: true,
+      
+    },
+    shu:{
+          type: Number,
+          unique:true,
+        required: true
+      
+    },
+    images:{
+          type: String,
+        required: true,
+        
+    },
+    brand:{
+          type: String,
+        required: true,
+     
+    },
+    category:{
+          type: String,
+        required: true,
+        minLength: 3
+    },
+},
+{timestamps: true},
+);
+module.exports = mongoose.model("product", ProductSchema);
