0f18903b9cfbffc658198cbbe9b2d555f35ca3a4
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-04-25 13:37:51 +0530

    msg

--- login_systems/BACKEND/controllers/cart.controller.js
+++ login_systems/BACKEND/controllers/cart.controller.js
@@ -3,26 +3,33 @@ const cartModel = require("../models/cart.model");
 
 
 // Add To Cart
-module.exports.AddToCart = (req , res)=>{
+module.exports.AddToCart = async(req , res)=>{
     try {
         const userId = req.user.id;
         const {item} = req.body;
         const Exist = await cartModel.findOne({userId});
-        const existProduct = Exist.items.map((val) =>{
-            const ids = val.productId;
-            return ids;
+        if (Exist){
+        const existProduct = Exist.items.map((val) => {
+          const ids = val.productId;
+          return ids;
         });
 
-        existProduct.forEach((e)=>{
-            if (e.equals(item.productId)){
-                return res.status(400).json({message:"Product Already Into Cart"});
-            }
+        existProduct.forEach((e) => {
+            console.log(e)
+          if (e.equals(item.productId)) {
+            return res
+              .status(400)
+              .json({ message: "Product Already Into Cart" });
+          }
         });
 
-        const cart = await cartService.addToCart({userId, items});
+        }
+
+        const cart = await cartService.addToCart({userId, item});
         return res.status(200).json({message:"Add item to cart successfully", cart});
 
     } catch (error) {
+        console.log(error)
         return res.status(400).json({message : error.message})
     }
 }
